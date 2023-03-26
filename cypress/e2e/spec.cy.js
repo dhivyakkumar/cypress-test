5ff4642f3d82c4badd3418fcb7844b40a92f0346
Dhivya Kumar <dhivya.kumar@sumup.com>
2023-03-26 13:40:29 +0200

    Fix test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('cypress spec', () => {
   it('passes', () => {
     cy.visit('https://storefront-vercel.sam-app.ro/de-AT/product-selection')
-    cy.get('[data-testid="product_card.card_reader.air.CALL_TO_ACTION"]1').click()
+    cy.get('[data-testid="product_card.card_reader.air.CALL_TO_ACTION"]').click()
   })
 })
